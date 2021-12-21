9cd7da131d82de15f0cf0a945894ef8621640207
Wala Zeidan <walazeidan123456789@gmail.com>
2021-12-21 16:48:08 +0000

    under construction

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Navbar from './Navbar'
-import About from './About'
-import Projects from './Projects'
-import Skills from './Skills'
-import Contact from './Contact'
+
 
 function App() {
   return (
     <div className="wrapper">
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
+      <h1>coming soon</h1>
     </div>
   )
 }
